d4dfff9039cd79886b0807adc08eadf7ac0f39d0
Aandeel <65628869+aandeel@users.noreply.github.com>
2026-04-16 14:24:19 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const redirectPath = new URLSearchParams(window.location.search).get('p')
+
+if (redirectPath) {
+  const nextPath = redirectPath.startsWith('/') ? redirectPath : `/${redirectPath}`
+  const nextUrl = `${nextPath}${window.location.hash}`
+  window.history.replaceState(null, '', nextUrl)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
